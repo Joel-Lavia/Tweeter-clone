3f8a07c7aa6a943c8ba09429d9e03663007fad2c
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-21 15:47:33 +0100

    integration du composant header et de la photo de profil

---
+++ src/components/TweetEditor.jsx
@@ -0,0 +1,12 @@
+import imgProfil from '../images/profile-photo.png'
+function TweetEditor(){
+    return (
+        <section>
+        <a href="#">
+        <img src={imgProfil} alt="profile-photo.png"/>
+        </a>
+        </section>
+
+    );
+}
+export default TweetEditor

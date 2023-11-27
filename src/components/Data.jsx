27e9ebc5816f5abc0dda1e346423de77cc822de6
Joel-Lavia <joeldisidi8@gmail.com>
2023-11-27 17:38:56 +0100

    Integrations des objets Tweet

---
+++ src/components/Data.jsx
@@ -0,0 +1,40 @@
+import group from '../images/Group.png';
+import sheet from '../images/tweet-image.png'
+function Data(){
+const data =[
+{
+    nomTweet :"CNN",
+    certification :group,
+    lienTweet:"@CNN . 7m",
+    tweetSetence:'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.'
+},
+{
+    nomTweet :"The New York Times",
+    certification :group,
+    lienTweet:"@nytimes . 2h",
+    tweetSetence:'Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land',
+    tweetimg :sheet
+},
+{
+    nomTweet :"Tweeter",
+    certification :group,
+    lienTweet:"@Tweeter . oct 29",
+    tweetSetence:'BIG NEWS lol jk still Twitter'   
+},
+{
+    nomTweet :"Tweeter",
+    certification :group,
+    lienTweet:"@Tweeter . oct 4",
+    tweetSetence:'hello literally everyone'   
+},
+{
+    nomTweet :"Tweeter",
+    certification :group,
+    lienTweet:"@Tweeter . oct 4",
+    tweetSetence:'hello literally everyone',
+    tweetimg:sheet   
+}
+]
+}
+
+export default Data;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import TweetEditor from "../components/TweetEditor";
+import Data from "../components/Data";
 
 function Home() {
   return (
     <main className="timeline">
     <Header />
     <TweetEditor />
+    <Data />
     </main>
   );
 }

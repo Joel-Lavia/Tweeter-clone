824f5232fcb897041083422392fe5378f7b75c16
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-03 18:27:28 +0100

    integration de differents tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -3,10 +3,20 @@ import Avatar from "./Avatar";
 function Twett() {
   const tweet = data.map((dataAll) => {
     return (
-      <div key={dataAll.id}>
+      <div key={dataAll.id} className="tweet">
         <Avatar avatarImg={dataAll.avatar} />
-        <div>
-          <h1 className="tweet-title-author">{dataAll.nomTweet}</h1>
+        <div className="tweet-body">
+          <p className="tweet-title">
+            <h1 className="tweet-title-author">{dataAll.nomTweet}</h1>
+            <a href="#">
+              <img src={dataAll.certification} alt="group" />
+            </a>
+            <span className="tweet-title-details">{dataAll.lienTweet}</span>
+          </p>
+          <p className="tweet-text">{dataAll.tweetSetence}</p>
+          <a href="#" className="tweet-image">
+            <img src={dataAll.tweetimg} alt="" />
+          </a>
         </div>
       </div>
     );

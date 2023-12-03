dc50c31f54d4d2b120804f967fe1e712fa7fc164
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-03 18:36:41 +0100

    interfration de commentaire dans le composant tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -4,8 +4,13 @@ function Twett() {
   const tweet = data.map((dataAll) => {
     return (
       <div key={dataAll.id} className="tweet">
+        {/*Composant des divers avatar de la page*/}
         <Avatar avatarImg={dataAll.avatar} />
+
+        {/*differents tweets de la page*/}
         <div className="tweet-body">
+        
+          {/*differents tweets-title de la page*/}
           <p className="tweet-title">
             <h1 className="tweet-title-author">{dataAll.nomTweet}</h1>
             <a href="#">
@@ -13,10 +18,14 @@ function Twett() {
             </a>
             <span className="tweet-title-details">{dataAll.lienTweet}</span>
           </p>
+          {/*differents phrase de la page*/}
           <p className="tweet-text">{dataAll.tweetSetence}</p>
+
+          {/*differents tweet-images de la page*/}
           <a href="#" className="tweet-image">
             <img src={dataAll.tweetimg} alt="" />
           </a>
+
         </div>
       </div>
     );

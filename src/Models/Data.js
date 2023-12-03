951bfe96df0f73163c2864b7c9a0355580d269db
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-03 20:33:06 +0100

    integration des boutton de tweet

--- src/Models/Data.js
+++ src/Models/Data.js
@@ -3,6 +3,10 @@ import sheet from "../images/tweet-image.png";
 import cnn from "../images/tweet-profile-photo.png";
 import nytimes from "../images/Tweet2.png";
 import twetterMascote from "../images/MascoteImg.png";
+import reply from "../images/Reply.png";
+import retweet from "../images/Retweet.png";
+import react from "../images/React.png";
+import btndowland from "../images/Buton.png";
 
 const data = [
   {
@@ -13,6 +17,18 @@ const data = [
     lienTweet: "@CNN . 7m",
     tweetSetence:
       'President Joe Biden touted a new agreement reached with the European Union to ease Trump-era tariffs on aluminum and steel as a "major breakthrough" that would serve to both strengthen the US steel industry and combat the global climate crisis.',
+    btn: {
+      replyImg: reply,
+      numbeeReply: 57,
+
+      retweetImg: retweet,
+      numberRetweet: 144,
+
+      reactImg: react,
+      numberReact: 123,
+
+      btnDowlandImg: btndowland,
+    },
   },
 
   {
@@ -24,6 +40,19 @@ const data = [
     tweetSetence:
       "Gardening boomed during the pandemic. Six Black writers share how it has helped them re-establish, and reimagine, a connection to cultivation and the land",
     tweetimg: sheet,
+
+    btn: {
+      replyImg: reply,
+      numbeeReply: 19,
+
+      retweetImg: retweet,
+      numberRetweet: 48,
+
+      reactImg: react,
+      numberReact: 482,
+
+      btnDowlandImg: btndowland,
+    },
   },
   {
     id: 3,
@@ -32,7 +61,21 @@ const data = [
     certification: group,
     lienTweet: "@Tweeter . oct 29",
     tweetSetence: "BIG NEWS lol jk still Twitter",
+
+    btn: {
+      replyImg: reply,
+      numbeeReply: "6.8k",
+
+      retweetImg: retweet,
+      numberRetweet: "36.k",
+
+      reactImg: react,
+      numberReact: "267.1k",
+
+      btnDowlandImg: btndowland,
+    },
   },
+
   {
     id: 4,
     nomTweet: "Tweeter",
@@ -40,7 +83,21 @@ const data = [
     certification: group,
     lienTweet: "@Tweeter . oct 4",
     tweetSetence: "hello literally everyone",
+
+    btn: {
+      replyImg: reply,
+      numbeeReply: "118.k",
+
+      retweetImg: retweet,
+      numberRetweet: "785.4k",
+
+      reactImg: react,
+      numberReact: "3.3M",
+
+      btnDowlandImg: btndowland,
+    },
   },
+
   {
     id: 5,
     avatar: twetterMascote,
@@ -49,6 +106,19 @@ const data = [
     lienTweet: "@Tweeter . oct 4",
     tweetSetence: "hello literally everyone",
     tweetimg: sheet,
+
+    btn: {
+      replyImg: reply,
+      numbeeReply: "118.7k",
+
+      retweetImg: retweet,
+      numberRetweet: "785.4k",
+
+      reactImg: react,
+      numberReact: "3.3M",
+
+      btnDowlandImg: btndowland,
+    },
   },
 ];
 
--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -9,7 +9,6 @@ function Twett() {
 
         {/*differents tweets de la page*/}
         <div className="tweet-body">
-        
           {/*differents tweets-title de la page*/}
           <p className="tweet-title">
             <h1 className="tweet-title-author">{dataAll.nomTweet}</h1>
@@ -25,7 +24,13 @@ function Twett() {
           <a href="#" className="tweet-image">
             <img src={dataAll.tweetimg} alt="" />
           </a>
-
+          {/*Tweetbutton*/}
+          <div className="tweet-actions">
+            <button className="tweet-action"><img src={dataAll.btn.replyImg} alt="replyimg" />{dataAll.btn.numbeeReply}</button>
+            <button className="tweet-action"><img src={dataAll.btn.retweetImg} alt="button retweet" />{dataAll.btn.numberRetweet}</button>
+            <button className="tweet-action"><img src={dataAll.btn.reactImg} alt="rection button" />{dataAll.btn.numberReact}</button>
+            <button className="tweet-action"><img src={dataAll.btn.btnDowlandImg} alt="button Dowland" /></button>
+          </div>
         </div>
       </div>
     );

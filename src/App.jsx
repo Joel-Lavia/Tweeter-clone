7f53d29a2987d5b3c179c091ab98a741ce5ce4de
Joel-Lavia <joeldisidi8@gmail.com>
2023-12-04 21:30:08 +0100

    integration de la sidebarLeft qui n'est pas encore finalise

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Home from "./pages/home.jsx";
 import Layout from "./components/layout.jsx";
+import SidebarLeft from "./components/SidebarLeft.jsx";
 
-import('./style/reset.css');
-import('./style/App.css');
+import("./style/reset.css");
+import("./style/App.css");
 
 export default function App() {
   return (
     <Layout>
+      <SidebarLeft />
       <Home />
+      
     </Layout>
   );
 }
---
+++ src/components/SidebarLeft.jsx
@@ -0,0 +1,36 @@
+import tweet from "../icons/Twitter.png";
+import home from "../icons/Home-Fill.png";
+import explor from "../icons/Explore.png";
+import notification from "../icons/Group.png";
+import messages from "../icons/Messages.png";
+import bookMark from "../icons/Bookmarks.png";
+import list from "../icons/Lists.png";
+import profilTweet from "../icons/Profile.png";
+import profilePhoto from "../images/profile-photo.png"
+import more from "../icons/More.png"
+import Avatar from "./Avatar";
+
+function SidebarLeft() {
+  return (
+    <section className="sidebarSection">
+
+     <div className="sidebar">
+      <button className="sidebarbtn"><img src={tweet} alt="btn Tweeter" /></button>
+      <button className="sidebarbtn"><img src={home} alt="btn home" />Home</button>
+      <button className="sidebarbtn"><img src={explor} alt="btn explor" />Explore</button>
+      <button className="sidebarbtn"><img src={notification} alt="btn notification" />Notifications</button>
+      <button className="sidebarbtn"><img src={messages} alt="btn messages" />Messages</button>
+      <button className="sidebarbtn"><img src={bookMark} alt="btn bookmarks" />Bookmarks</button>
+      <button className="sidebarbtn"><img src={list} alt="btn twett list" />Lists</button>
+      <button className="sidebarbtn"><img src={profilTweet} alt="btn profil tweet" />Profil</button>
+      <button className="sidebarbtn"><img src={more} alt="btn more" />More</button>
+      <button className="button">Tweet</button>
+     </div>
+    
+ <div>
+ <Avatar avatarImg={profilePhoto}/>
+ </div>
+    </section>
+  );
+}
+export default SidebarLeft;

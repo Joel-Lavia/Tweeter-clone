5095393c9b0176a47d70818dd5d6eebb49dc3508
Joel-Lavia <joeldisidi8@gmail.com>
2024-01-08 18:11:32 +0100

    integration et finalisation de la partie htlm sans css du composant follow

--- src/components/Follow.jsx
+++ src/components/Follow.jsx
@@ -1,14 +1,18 @@
 import ProfilSetting from "./ProfilSetting";
 import imgProfilCnn from '../images/tweet-profile-photo.png'
-import privateImg from "../images/Private.png"
+import group from "../images/Group.png"
+import imgProfilnytm from "../images/Tweet2.png"
+import imgMascotetweeter from '../images/MascoteImg.png'
 
 
 function Follow() {
 return(
 <section>
-<ProfilSetting imgProfilSidebaLeft={imgProfilCnn} namProfil={'CNN'} details={'@CNN'} iconProfil={privateImg}/>
-<ProfilSetting imgProfilSidebaLeft={imgProfilCnn} namProfil={'CNN'} details={'@CNN'} iconProfil={privateImg}/>
-<ProfilSetting imgProfilSidebaLeft={imgProfilCnn} namProfil={'CNN'} details={'@CNN'} iconProfil={privateImg}/>
+<ProfilSetting imgProfilSidebaLeft={imgProfilCnn} namProfil={'CNN'} details={'@CNN'} iconProfil={group}/>
+<ProfilSetting imgProfilSidebaLeft={imgMascotetweeter} namProfil={'Tweetter'} details={'@Twetter'} iconProfil={group}/>
+<ProfilSetting imgProfilSidebaLeft={imgProfilnytm} namProfil={'The New York Times'} details={'@nytimes'} iconProfil={group}/>
+
+
 </section>
 );
 }

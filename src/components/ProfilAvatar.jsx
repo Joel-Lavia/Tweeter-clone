020b622466a077e657ad892037c8460aa903db8c
Hol418 <joeldisidi8@gmail.com>
2024-01-11 20:47:23 +0100

    reorganisation du du style de la page profil

--- src/components/ProfilAvatar.jsx
+++ src/components/ProfilAvatar.jsx
@@ -3,22 +3,14 @@ import profilePhoto from '../images/profile-photo.png'
 import BtnEditProfil from "./BtnEditProfil";
 function ProfilAvatar() {
  return(
-<section className="timeline">
-<div className="fleximgProfil ">
+<main className="timeline">
 
-<div className="sectionProfilAvatar">
 <Avatar avatarImg={profilePhoto}/>
 <p className="profilSibare">Bradley Ortiz</p>
 <p className="tweet-title-details">@bradley_</p>
-</div>
-<div className="editProfilbtn">
 <BtnEditProfil/>
-</div>
 
-</div>
-
-
-</section>
+</main>
  );   
 }
 export default ProfilAvatar

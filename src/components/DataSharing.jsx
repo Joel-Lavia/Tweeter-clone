19979c3888b685a49b16f112fdf9ca8b4888bec3
Joel-Lavia <joeldisidi8@gmail.com>
2024-03-27 21:02:11 +0100

    creation du deuxieme depot

--- src/components/DataSharing.jsx
+++ src/components/DataSharing.jsx
@@ -8,13 +8,17 @@ function DataSharing({ children }) {
   const [dataValue, setDataValue] = useState(Data);
   // const [dataUsers,setDataUsers] = useState(Data.currentUser);
 
-  useEffect(() => {
-    axios.get("http://localhost:3000/tweets")
-    .then((response) => {
-      setDataValue(response.data);
-      console.log(response);
-    });
-  }, []);
+ const data = () => {
+  useEffect(() => {axios.get("http://localhost:3000/tweets")
+  .then((response) => {
+    setDataValue(response.data);
+    console.log(response);
+  });
+}, []);
+ }
+ console.log(data());
+ 
+  
 
   const addTweet = (newTweet) => {
     const dataValueTemporary = dataValue;
